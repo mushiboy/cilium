404baf07d6a6707ead3c6d23f5f173e90f71e5c5
mushiboy <mushi025@gmail.com>
2025-03-17 12:58:46 +0000

    metrics: add new BPF attachment statistics to SpanStat

--- pkg/datapath/loader/metrics/metrics.go
+++ pkg/datapath/loader/metrics/metrics.go
@@ -10,16 +10,22 @@ import (
 // SpanStat is a statistics structure for storing metrics related to datapath
 // load operations.
 type SpanStat struct {
-	BpfCompilation spanstat.SpanStat
-	BpfWaitForELF  spanstat.SpanStat
-	BpfLoadProg    spanstat.SpanStat
+	BpfCompilation          spanstat.SpanStat
+	BpfWaitForELF           spanstat.SpanStat
+	BpfLoadProg             spanstat.SpanStat
+	BpfAttachCiliumHost     spanstat.SpanStat
+	BpfAttachCiliumNet      spanstat.SpanStat
+	BpfAttachNetworkDevices spanstat.SpanStat
 }
 
 // GetMap returns a map of statistic names to stats
 func (s *SpanStat) GetMap() map[string]*spanstat.SpanStat {
 	return map[string]*spanstat.SpanStat{
-		"bpfCompilation": &s.BpfCompilation,
-		"bpfWaitForELF":  &s.BpfWaitForELF,
-		"bpfLoadProg":    &s.BpfLoadProg,
+		"bpfCompilation":          &s.BpfCompilation,
+		"bpfWaitForELF":           &s.BpfWaitForELF,
+		"bpfLoadProg":             &s.BpfLoadProg,
+		"bpfAttachCiliumHost":     &s.BpfAttachCiliumHost,
+		"bpfAttachCiliumNet":      &s.BpfAttachCiliumNet,
+		"bpfAttachNetworkDevices": &s.BpfAttachNetworkDevices,
 	}
 }
