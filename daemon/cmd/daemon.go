2a9b893ff44ff906453c5f1d151ccdb71d75cb56
mushiboy <mushi025@gmail.com>
2025-04-29 10:57:39 +0100

    daemon: increase semaphore weight for build endpoint concurrency

--- daemon/cmd/daemon.go
+++ daemon/cmd/daemon.go
@@ -370,7 +370,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		ctx:               ctx,
 		clientset:         params.Clientset,
 		db:                params.DB,
-		buildEndpointSem:  semaphore.NewWeighted(int64(numWorkerThreads() * 2)),
+		buildEndpointSem:  semaphore.NewWeighted(int64(numWorkerThreads() * 16)),
 		compilationLock:   params.CompilationLock,
 		mtuConfig:         params.MTU,
 		directRoutingDev:  params.DirectRoutingDevice,
